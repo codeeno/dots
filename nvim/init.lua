9541c7c48eac3b4648183bd8bae963461f4d5360
codeeno <sebastian@kleboth.de>
2021-12-31 03:37:51 +0100

    add nvim configs

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+require('options')
+require('plugins')
+require('keybinds')
+require('treesitter-config')
+require('telescope-config')
+require('bufferline-config')
+require('nvim-tree-config')
+require('nvim-lualine-config')
+require('nvim-comment-config')
+
+require('catppuccin-config')
+require('tokyonight-config')
+
+vim.cmd[[colorscheme tokyonight]]
---
+++ nvim/lua/options/init.lua
@@ -0,0 +1,11 @@
+vim.opt.mouse         = "a"
+
+vim.opt.termguicolors = true
+vim.opt.nu            = true
+vim.opt.hidden        = true
+
+vim.opt.shiftwidth    = 2
+vim.opt.softtabstop   = 2
+vim.opt.expandtab     = true
+vim.opt.splitbelow    = true
+vim.opt.splitright    = true
