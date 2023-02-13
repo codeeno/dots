1e2a07e2fa2c99b087e59e385c2f34232730c360
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2023-02-13 01:38:05 +0100

    add tokyonight

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,11 +1,23 @@
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     config = function()
+--       -- load the colorscheme here
+--       vim.cmd([[colorscheme catppuccin-macchiato]]) -- latte, frappe, macchiato, mocha
+--     end,
+--   },
+-- }
+
 return {
   {
-    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme catppuccin-macchiato]]) -- latte, frappe, macchiato, mocha
+      vim.cmd([[colorscheme tokyonight]]) -- latte, frappe, macchiato, mocha
     end,
   },
 }
