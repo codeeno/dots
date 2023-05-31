13c4aeb8b86c0a7595ff2e30a6a11f46a49b5573
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2023-05-31 10:21:57 +0200

    add presence

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -2,5 +2,8 @@ return {
   "ggandor/leap.nvim",
   keys = {
     { "x", false },
+    -- TODO: The below keybindsings don't seem to work, need to figure this out
+    { "<leader>j", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+    { "<leader>k", mode = { "n", "x", "o" }, desc = "Leap backward to" },
   },
 }
