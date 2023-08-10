5a08cc4023032421b1456830ab8e24946b3ee96e
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2023-08-10 10:50:11 +0200

    add some luadev configs

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,23 +1,51 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavor = "latte",
+      integrations = {
+        alpha = true,
+        cmp = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
+}
+
 -- return {
 --   {
---     "catppuccin/nvim",
+--     "folke/tokyonight.nvim",
 --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
 --     priority = 1000, -- make sure to load this before all the other start plugins
 --     config = function()
 --       -- load the colorscheme here
---       vim.cmd([[colorscheme catppuccin-macchiato]]) -- latte, frappe, macchiato, mocha
+--       vim.cmd([[colorscheme tokyonight]]) -- latte, frappe, macchiato, mocha
 --     end,
 --   },
--- }
-
-return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]]) -- latte, frappe, macchiato, mocha
-    end,
-  },
-}
+-- latte}
