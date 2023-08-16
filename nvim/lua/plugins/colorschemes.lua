bfa0ef8ed59d80030835813e39fa26afd741fd69
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2023-08-16 16:25:16 +0200

    add gopls config

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -4,7 +4,7 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
-      flavor = "latte",
+      flavor = "macchiato",
       integrations = {
         alpha = true,
         cmp = true,
