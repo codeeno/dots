1dbc1a06795bb0c08ba25c03b0e8ab9798f86009
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2023-10-01 11:03:55 +0200

    update null-ls => none-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     dependencies = { "mason.nvim" },
     opts = function()
       local nls = require("null-ls")
