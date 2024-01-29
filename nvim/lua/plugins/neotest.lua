a854ee96d8d77eb3e8fa042fe36344f024d7c2f9
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2024-01-29 13:26:03 +0100

    re-add neotest config

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+    },
+    keys = {
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
+    opts = function(_, opts)
+      -- Adapters
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test -- --color",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+      table.insert(opts.adapters, require("neotest-vitest"))
+
+      -- General Config
+      opts.output_panel = {
+        open = "vsplit | vertical resize 80",
+      }
+    end,
+  },
+}
