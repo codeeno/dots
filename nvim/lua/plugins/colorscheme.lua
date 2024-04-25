2666973b65eaa1e279a29936c549baaf6dcc47cd
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2024-04-25 17:18:23 +0200

    update stuff

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -38,9 +38,15 @@ return {
     },
   },
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight-moon",
     },
   },
 }
