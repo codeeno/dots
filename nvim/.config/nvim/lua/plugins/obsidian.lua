6520d1ea4c28098dacfa40eb993dfaac0e3fe395
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2025-04-08 18:09:27 +0200

    improve obsidian nvim config

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,41 +1,41 @@
--- return {
---   "epwalsh/obsidian.nvim",
---   lazy = true,
---   event = { "BufReadPre " .. vim.fn.expand("~") .. "/Notes/obsidian-notes/**.md" },
---   dependencies = {
---     -- Required.
---     "nvim-lua/plenary.nvim",
---   },
---   opts = {
---     dir = "~/Notes/obsidian-notes", -- no need to call 'vim.fn.expand' here
---   },
--- }
-
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
   -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies
   },
   opts = {
-    dir = "~/Notes/obsidian-notes", -- no need to call 'vim.fn.expand' here
-    -- workspaces = {
-    --   {
-    --     name = "personal",
-    --     path = "~/vaults/personal",
-    --   },
-    -- },
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/Notes/obsidian-notes",
+      },
+    },
+    daily_notes = {
+      folder = "Daily",
+      date_format = "YYYY/MM/YYYY-MM-DD-dddd",
+      template = "Daily",
+    },
+    templates = {
+      folder = "_templates",
+    },
+    mappings = {
+      ["<leader>oc"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+    },
   },
 }
