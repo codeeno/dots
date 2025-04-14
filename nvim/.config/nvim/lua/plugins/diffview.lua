f791f97562a323ba9f70bb7d01b7983def713de1
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2025-04-14 13:14:50 +0200

    add diffview keybind for comparing current branch to main

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,15 @@
 return {
-  "sindrets/diffview.nvim",
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons", lazy = true },
+    },
+    keys = {
+      {
+        "<leader>gdm",
+        "<cmd>DiffviewOpen main..HEAD<cr>",
+        desc = "Open Diffview comparing current branch to main",
+      },
+    },
+  },
 }
