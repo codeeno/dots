1a6ad327b24969b13e03efe4bb33a950304f81fc
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2025-04-20 14:20:46 +0200

    pin node path for copilot lua due to the binary being lazy-loaded in zsh

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v20.12.0/bin/node",
     filetypes = {
       yaml = true, -- overrides default
     },
