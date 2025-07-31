e8af1a69d083b3aefaa48e14e8d565f725eec257
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2025-07-31 13:13:24 +0200

    get rid of copilot custom keybinds

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -4,13 +4,5 @@ return {
     filetypes = {
       yaml = true, -- overrides default
     },
-    suggestion = {
-      auto_trigger = true,
-      keymap = {
-        accept = "<C-j>",
-        next = "<C-]>",
-        prev = "<C-[>",
-      },
-    },
   },
 }
