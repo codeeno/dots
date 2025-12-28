39388e2d11a71289e89f9e8d76ae4517a00dc8bf
Sebastian Kleboth <sebastian.kleboth@valiton.com>
2025-12-28 20:10:52 +0100

    add arch-wsl host

---
+++ hosts/arch-wsl/home.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+let
+  flakePath = "~/projects/dots";
+  flakeConfig = "eeno@pc";
+in
+{
+  imports = [
+    ../../modules/common/terminal
+    ../../modules/common/programs/kitty.nix
+  ];
+
+  home = {
+    username = "eeno";
+    homeDirectory = "/home/eeno";
+    stateVersion = "25.11";
+
+    packages = with pkgs; [ ];
+  };
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    zsh.shellAliases = {
+      hm = "home-manager switch --flake ${flakePath}#${flakeConfig}";
+      hmd = "home-manager switch --flake ${flakePath}#${flakeConfig} --dry-run";
+
+      pbcopy = "xsel --clipboard --input";
+      pbpaste = "xsel --clipboard --output";
+    };
+  };
+}
