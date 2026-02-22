5d2cbf0915c5855cd75fd1fc392c26797fffcaeb
Sebastian Kleboth <sebastian@kleboth.de>
2026-02-22 14:46:04 +0100

    add zellij module

---
+++ modules/common/terminal/zellij.nix
@@ -0,0 +1,16 @@
+{
+  programs.zellij = {
+    enable = true;
+  };
+
+  xdg.configFile."zellij/config.kdl".text = ''
+    theme "tokyo-night-storm"
+
+    keybinds {
+      normal {
+        bind "Alt n" { NewTab; SwitchToMode "Normal"; }
+        bind "Alt w" { CloseTab; SwitchToMode "Normal"; }
+      }
+    }
+  '';
+}
